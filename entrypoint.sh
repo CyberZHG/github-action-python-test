c717fb384d3001667cad0756bd31461a5bdab157
CyberZHG <CyberZHG@gmail.com>
2019-03-26 19:11:59 +0800

    Install requirements

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
-echo "nosetests $@"
-nosetests $@
+args=$@
+shopt -s nullglob
+requirements=(requirement*.txt)
+for requirement in "${requirements[@]}"
+do
+    command="pip install -r $requirement"
+    echo ${command}
+    ${command}
+done
+command="nosetests $args"
+echo ${command}
+${command}
